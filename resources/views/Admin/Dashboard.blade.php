ad6e4c39239d3868bd5fa8f3664809827cda5e40
Thaitea16 <idangaming66@gmail.com>
2023-11-11 11:45:30 +0700

    ya

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -33,8 +33,6 @@
 
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
-                <!-- /.dropdown -->
-                <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
@@ -54,7 +52,7 @@
         </nav>
         <!--/. NAV TOP  -->
         <nav class="navbar-default navbar-side" role="navigation">
-		<div id="sideNav" href=""><i class="fa fa-caret-right"></i></div>
+		
             <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
 
@@ -122,13 +120,9 @@
                 <div class="row">
                     <div class="col-md-12">
                         <h1 class="page-header">
-                            Dashboard <small>Selamat Datang Di Pembayaran Spp</small>
+                            Dashboard <small>Selamat Dadang</small>
                         </h1>
-						<ol class="breadcrumb">
-  <li><a href="#">Home</a></li>
-  <li><a href="#">Library</a></li>
-  <li class="active">Data</li>
-</ol>
+						
                     </div>
                 </div>
 				
@@ -144,7 +138,7 @@
                             </div>
                             <div class="panel-right">
 								<h3>8,457</h3>
-                               <strong> Daily Visits</strong>
+                               <strong> Jumlah Siswa</strong>
                             </div>
                         </div>
                     </div>
@@ -156,7 +150,7 @@
                                 
                             <div class="panel-right">
 							<h3>52,160 </h3>
-                               <strong> Sales</strong>
+                               <strong>Jumlah Bayar</strong>
 
                             </div>
                         </div>
@@ -169,7 +163,7 @@
                             </div>
                             <div class="panel-right">
 							 <h3>15,823 </h3>
-                               <strong> Comments </strong>
+                               <strong></strong>
 
                             </div>
                         </div>
@@ -182,7 +176,7 @@
                             </div>
                             <div class="panel-right">
 							<h3>36,752 </h3>
-                             <strong>No. of Visits</strong>
+                             <strong>Jumlah Kelas</strong>
 
                             </div>
                         </div>
@@ -233,55 +227,12 @@
 		</div><!--/.row-->
 			
 				
-                
-               
-                <!-- /. ROW  -->
 
 	   
 				
 				
 				
-                <div class="row">
-                    <div class="col-md-4 col-sm-12 col-xs-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                Tasks Panel
-                            </div>
-                            <div class="panel-body">
-                                <div class="list-group">
-
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">7 minutes ago</span>
-                                        <i class="fa fa-fw fa-comment"></i> Commented on a post
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">16 minutes ago</span>
-                                        <i class="fa fa-fw fa-truck"></i> Order 392 shipped
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">36 minutes ago</span>
-                                        <i class="fa fa-fw fa-globe"></i> Invoice 653 has paid
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">1 hour ago</span>
-                                        <i class="fa fa-fw fa-user"></i> A new user has been added
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">1.23 hour ago</span>
-                                        <i class="fa fa-fw fa-user"></i> A new user has added
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">yesterday</span>
-                                        <i class="fa fa-fw fa-globe"></i> Saved the world
-                                    </a>
-                                </div>
-                                <div class="text-right">
-                                    <a href="#">More Tasks <i class="fa fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+               
                     <div class="col-md-8 col-sm-12 col-xs-12">
 
                         <div class="panel panel-default">
@@ -355,7 +306,6 @@
                 <!-- /. ROW  -->
 			
 		
-				
 				
         
 				</footer>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('index', function () {
+    return view('Admin.index');
 });
 Route::get('das', function () {
     return view('Admin.Dashboard');
+});
+Route::get('chart', function () {
+    return view('Admin.chart');
+});
+Route::get('empty', function () {
+    return view('Admin.empty');
+});
+Route::get('form', function () {
+    return view('Admin.form');
+});
+Route::get('ui', function () {
+    return view('Admin.ui-elements');
+});
+Route::get('table', function () {
+    return view('Admin.table');
+});
+Route::get('tabpanel', function () {
+    return view('Admin.tab-panel');
 });
